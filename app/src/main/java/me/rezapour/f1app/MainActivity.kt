617fd3ecd061b84b3919b3651e285b9eac6e22d4
Mohammadreza Abdollahpour <reza.pour1992@gmail.com>
2025-07-01 20:15:07 +0200

    config: implement ksp and dagger

--- app/src/main/java/me/rezapour/f1app/MainActivity.kt
+++ app/src/main/java/me/rezapour/f1app/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import me.rezapour.f1app.ui.theme.F1AppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/me/rezapour/f1app/MyApplication.kt
@@ -0,0 +1,8 @@
+package me.rezapour.f1app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication:Application() {
+}
